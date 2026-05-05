16f219bdd24a433b908eb8cf2f698eeca996477c
waseemkhanjdpr1902 <waseemkhanjdpr@gmail.com>
2026-05-05 15:51:00 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,18 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  // STRICTLY only look at the app folder. This ignores your root /css and /js folders.
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./css/**/*.{css}",
+    "./js/**/*.{js,ts,jsx,tsx}",
+    "./lib/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
 export default config;
