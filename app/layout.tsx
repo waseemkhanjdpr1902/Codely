803ace98f417339358a5e10cd38057ad5ecfc9f3
waseemkhanjdpr1902 <waseemkhanjdpr@gmail.com>
2026-05-05 15:29:32 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
-import "./globals.css"; // ADD THIS LINE
+import "./globals.css";
+// If you have a file like css/style.css, link it like this:
+import "../css/style.css"; 
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
